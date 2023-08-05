ab78df4eb6ca4e17d287602ce80dbc38a1fcb222
KostiantynSS <storoghenko94@gmail.com>
2023-08-05 18:11:34 +0300

    1

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,27 @@
 import axios from 'axios';
 
-
+import SlimSelect from 'slim-select'
+import 'slim-select/dist/slimselect.css'
+new SlimSelect({
+  select: '#selectElement'
+})
 // axios.defaults.headers.common['x-api-key'] = 'live_flQsdmawXVHOEszZKd0VDFO3Nlsnte0OFf9SJwDCYz8Cq2lt0EpZwPMk51cGmD3e';
 // axios.defaults.baseURL = 'https://api.thecatapi.com/v1'
 
+/* <div class="photo-card">
+  <img src="" alt="" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+    </p>
+  </div>
+</div> */
