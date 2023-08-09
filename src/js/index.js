ca641feed02e73ad1296dd01df60b635d375c5a6
KostiantynSS <storoghenko94@gmail.com>
2023-08-09 21:18:57 +0300

    1

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,13 @@
 import axios from 'axios';
+import Notiflix from 'notiflix';
 
+const form = document.querySelector('#search-form')
+const gallery = document.querySelector('.gallery')
+const loadMoreBtn = document.querySelector('.load-more')
+
+form.addEventListener('submit', onSubmit)
+loadMoreBtn.addEventListener('click', handleLoadMoreBtn)
+let currentPage = 1;
 
 const searchParams = new URLSearchParams({
   key:'38665853-fe99969bd23bb921fc896ab74',
@@ -7,42 +15,64 @@ const searchParams = new URLSearchParams({
   orientation:'horizontal',
   safesearch: true,
 per_page: 40, 
-page: 1});
+page: currentPage
+});
   axios.defaults.baseURL = 
-  `https://pixabay.com/api/?`
+  `https://pixabay.com/api/?`;
 
-const form = document.querySelector('#search-form')
-const gallery = document.querySelector('.gallery')
-form.addEventListener('submit', onSubmit)
-
-function onSubmit(e) { 
+async function onSubmit(e) { 
+  gallery.innerHTML= ''
   e.preventDefault()
-const searchRequest = form.elements[0].value
+  const searchRequest = form.elements[0].value.trim()
   if(searchRequest)
 {searchParams.set('q', `${searchRequest}`)}
-  axios('',{params:searchParams}).then(resp => {
-    const photoArr = resp.data.hits;
-    photoArr.map(result => gallery.insertAdjacentHTML('beforeend', createMarkup(result)) )
-    console.log(photoArr)})
+axiosPhotos()
+  
   }
-function createMarkup(result){return `<div class="photo-card">
-<img src="${result.webformatURL}" alt="${result.tags}" loading="lazy" />
+function createMarkup({webformatURL, tags, likes, views, comments, downloads}){
+  return `<div class="photo-card">
+<img src="${webformatURL}" alt="${tags}" loading="lazy" />
 <div class="info">
   <p class="info-item">
     <b>Likes </b>
-    <b>${result.likes}</b>
+    <b>${likes}</b>
   </p>
   <p class="info-item">
     <b>Views </b>
-    <b>${result.views}</b>
+    <b>${views}</b>
   </p>
   <p class="info-item">
     <b>Comments </b>
-    <b>${result.comments}</b>
+    <b>${comments}</b>
   </p>
   <p class="info-item">
     <b>Downloads </b>
-    <b>${result.downloads}</b>
+    <b>${downloads}</b>
   </p>
 </div>
 </div>`}
+function handleLoadMoreBtn(){
+  currentPage +=1;
+  searchParams.set('page', currentPage)
+ axiosPhotos()
+
+}
+
+
+
+  // console.log(currentPage)
+    // console.log(searchParams.get('page'));
+    // console.log('total:', resp.data.total);
+    // console.log('hits',resp.data.totalHits);
+    async function axiosPhotos(){await axios('',{params:searchParams})
+    .then(resp => {
+      
+      totalPages = Math.ceil(resp.data.total / searchParams.get('per_page'));
+      totalPages <= 1 || totalPages === currentPage ? loadMoreBtn.hidden = true: loadMoreBtn.hidden = false
+     
+      const photoArr = resp.data.hits;
+  if(photoArr.length === 0){Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again')}
+  if (totalPages === currentPage){Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.")}
+      photoArr.map(result => 
+        gallery.insertAdjacentHTML('beforeend', createMarkup(result)) )
+      }).catch(err => console.log(err))}
